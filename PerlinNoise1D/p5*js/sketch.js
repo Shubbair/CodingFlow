83552d5c58564699bce845f842e20681a9438b95
Hussain <e05161355@s.uokerbala.edu.iq>
2022-01-08 22:37:19 +0300

    Upload p5.js Code

---
+++ PerlinNoise1D/p5*js/sketch.js
@@ -0,0 +1,25 @@
+let inc = 0.01;
+let start = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(51);
+  
+  stroke(255);
+  noFill();
+  beginShape();
+  let xoff = start;
+  for (let x = 0; x < width; x++) {
+    stroke(255);
+    let y = noise(xoff) * height;
+    vertex(x, y);
+    
+    xoff += inc;
+  }
+  endShape();
+  
+  start += inc;  
+}
