c4857c8af30d6aa15d08c29cca08453ff3dd1304
Hussain <e05161355@s.uokerbala.edu.iq>
2022-01-09 22:36:48 +0300

    Add files via upload

---
+++ LorenzSystem/p5*js/sketch.js
@@ -0,0 +1,44 @@
+let x = 0.01;
+let y = 0;
+let z = 0;
+
+let a = 10;
+let b = 28;
+let c = 8.0 / 3.0;
+
+let points = [];
+
+
+function setup() {
+  createCanvas(800, 600, WEBGL);
+}
+
+function draw() {
+  background(0);
+
+  let dt = 0.01;
+  let dx = (a * (y - x)) * dt;
+  let dy = (x * (b - z) - y) * dt;
+  let dz = (x * y - c * z) * dt;
+  x = x + dx;
+  y = y + dy;
+  z = z + dz;
+
+  points.push(new p5.Vector(x, y, z));
+
+  translate(0, 0, -80);
+  let camX = map(mouseX, 0, width, -200, 200);
+  let camY = map(mouseY, 0, height, -200, 200);
+  camera(camX, camY, (height / 2.0) / tan(PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
+
+  scale(5);
+  stroke(255);
+  noFill();
+
+  beginShape();
+  for (let v of points) {
+    stroke(255);
+    vertex(v.x, v.y, v.z);
+  }
+  endShape();
+}
